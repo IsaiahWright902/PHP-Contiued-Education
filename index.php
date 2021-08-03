e760f89b3cf227ebd2cbcfeb1e151d5ad1bd9d69
IsaiahWright902 <icwright902@gmail.com>
2021-08-03 11:41:24 -0400

    init

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php 
+// $stringOne = 'my email is ';
+// $stringTwo= 'mario123@fake.com';
+//echo $stringOne.$stringTwo;
+// $name = 'mario';
+// echo 'Hey, my name is' . $name;
+//  echo "Hey my name is $name"
+// echo "the ninja screamed \"whaaaaa\"";
+// echo 'the ninja screamed "Whaaaaa"';
+// echo $name[0]
+// echo strlen($name);
+// echo strtoupper($name);
+// echo strtolower($name);
+// echo str_replace('m', 'w', $name);
+
+$radius = 25;
+$pi = 3.14;
+
+// echo $pi * $radius**2;
+// echo 2 * (4 + 9) / 3;
+// $radius--;
+// echo $radius
+
+// $age = 20;
+// $age -= 10;
+// echo $age
+
+// Number Functions
+// echo ceil($pi);
+echo pi();
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first PHP file</title>
+</head>
+<body>
+
+<h1><?php echo 'Hello World' ?></h1>
+
+
+    
+</body>
+</html>
